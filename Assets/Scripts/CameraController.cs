34bb5cc3d5a9115b6425e83644c425ab78f1941e
Daniel L <daniel.lachica82@gmail.com>
2018-06-24 16:52:31 +0800

    Initial commit

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject player;
+	private Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		offset = transform.position - player.transform.position; //camera position - player position
+	}
+	
+	// Update is called once per frame
+	// void Update () {
+		
+	// }
+
+	// LateUpdate() runs every frame, but is guaranteed to run after all items have been processed in Update()
+	void LateUpdate() {
+		transform.position = player.transform.position + offset; //move camera with player
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// Update is called once per frame
+	void Update () {
+		// Time.deltaTime rotates the object per second, not per frame
+		transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+	}
+}
